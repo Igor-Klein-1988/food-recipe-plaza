3d5b3a9cdcfb72bc62d942ddd42b0e579e7ab07b
Igor Klein <igorklein1988@gmail.com>
2023-10-03 20:57:36 +0200

    create app

---
+++ src/app/components/RecipeTitle/RecipeTitle.tsx
@@ -0,0 +1,23 @@
+import { FC } from "react"
+
+import style from "./RecipeTitle.module.css"
+
+interface RecipeInterface {
+  data: {
+    recipe: {
+      label: string
+      image: string
+    }
+  }
+}
+
+const RecipeTitle: FC<RecipeInterface> = ({ data }): JSX.Element => {
+  return (
+    <div className={style.recipeTile}>
+      <img className={style.recipeTile__img} src={data.recipe.image} alt="" />
+      <p className={style.recipeTile__name}>{data.recipe.label}</p>
+    </div>
+  )
+}
+
+export default RecipeTitle
