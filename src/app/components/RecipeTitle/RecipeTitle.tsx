35ada3339c47bbf7f495b75dc5705b62afa7ebe6
Igor Klein <igorklein1988@gmail.com>
2023-10-04 11:33:13 +0200

    add eslint

--- src/app/components/RecipeTitle/RecipeTitle.tsx
+++ src/app/components/RecipeTitle/RecipeTitle.tsx
@@ -1,23 +1,21 @@
-import { FC } from "react"
+import { FC } from 'react'
 
-import style from "./RecipeTitle.module.css"
+import style from './RecipeTitle.module.css'
 
 interface RecipeInterface {
-  data: {
-    recipe: {
-      label: string
-      image: string
-    }
-  }
+	data: {
+		recipe: {
+			label: string
+			image: string
+		}
+	}
 }
 
-const RecipeTitle: FC<RecipeInterface> = ({ data }): JSX.Element => {
-  return (
-    <div className={style.recipeTile}>
-      <img className={style.recipeTile__img} src={data.recipe.image} alt="" />
-      <p className={style.recipeTile__name}>{data.recipe.label}</p>
-    </div>
-  )
-}
+const RecipeTitle: FC<RecipeInterface> = ({ data }): JSX.Element => (
+	<div className={style.recipeTile}>
+		<img className={style.recipeTile__img} src={data.recipe.image} alt="" />
+		<p className={style.recipeTile__name}>{data.recipe.label}</p>
+	</div>
+)
 
 export default RecipeTitle
